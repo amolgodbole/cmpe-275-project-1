e9f84af60adf98b525ce082487bb31566bfdf4f5
nandishsb@gmail.com <nandishsb@gmail.com@50490843-ca8c-0344-2283-0fafcc131a05>
2012-02-24 00:48:50 +0000

    Initail Commit

---
+++ StockExtract/src/com/extract/ExtractModule.java
@@ -0,0 +1,13 @@
+package com.extract;
+
+public class ExtractModule {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ StockExtract/src/com/extract/StockKey.java
@@ -0,0 +1,33 @@
+package com.extract;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class StockKey implements Serializable{
+
+	private String stockDate;
+	private String ticker;
+	
+	public StockKey(String stockDate, String ticker)
+	{
+		this.stockDate = stockDate;
+		this.ticker = ticker;
+	}
+	
+	public String getStockDate() {
+		return stockDate;
+	}
+	public void setStockDate(String stockDate) {
+		this.stockDate = stockDate;
+	}
+	public String getTicker() {
+		return ticker;
+	}
+	public void setTicker(String ticker) {
+		this.ticker = ticker;
+	}
+	
+}
