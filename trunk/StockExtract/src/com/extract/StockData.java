057557db716aa9a076728b0b3ef87044eec29736
nandishsb@gmail.com <nandishsb@gmail.com@50490843-ca8c-0344-2283-0fafcc131a05>
2012-02-24 00:48:50 +0000

    Initail Commit

---
+++ trunk/StockExtract/src/com/extract/StockData.java
@@ -0,0 +1,63 @@
+package com.extract;
+
+import java.util.Date;
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table (name="stockdata")
+public class StockData {
+
+	StockKey stockKey;
+
+	private double open;
+	private double high;
+	private double low;
+	private double close;
+	private int volume;
+	
+	
+	@Id
+	@Embedded
+	public StockKey getStockKey() {
+		return stockKey;
+	}
+	public void setStockKey(StockKey stockKey) {
+		this.stockKey = stockKey;
+	}
+	public double getOpen() {
+		return open;
+	}
+	public void setOpen(double open) {
+		this.open = open;
+	}
+	public double getHigh() {
+		return high;
+	}
+	public void setHigh(double high) {
+		this.high = high;
+	}
+	public double getLow() {
+		return low;
+	}
+	public void setLow(double low) {
+		this.low = low;
+	}
+	public double getClose() {
+		return close;
+	}
+	public void setClose(double close) {
+		this.close = close;
+	}
+	public int getVolume() {
+		return volume;
+	}
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	
+}
