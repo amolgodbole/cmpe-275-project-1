f73db24c1c377cbb061defab8801cf619dd5b14d
nandishsb@gmail.com <nandishsb@gmail.com@50490843-ca8c-0344-2283-0fafcc131a05>
2012-02-27 02:14:32 +0000

    Sample JMS..run sender.java

---
+++ trunk/SendJMS/ejbModule/com/jms/Sender.java
@@ -0,0 +1,71 @@
+package com.jms;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+                                                                           
+import javax.jms.BytesMessage;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.QueueSender;
+import javax.jms.DeliveryMode;
+import javax.jms.QueueSession;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+                                                                           
+public class Sender
+{
+    public static void main(String[] args) throws Exception
+    {
+       // get the initial context
+    	Properties properties = new Properties();
+		properties.put(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
+		properties.put(Context.URL_PKG_PREFIXES, "org.jnp.interfaces");
+		properties.put(Context.PROVIDER_URL, "localhost");
+   
+		InitialContext ctx = new InitialContext(properties);
+    	
+    	//InitialContext ctx = new InitialContext();
+                                                                          
+       // lookup the queue object
+       Queue queue = (Queue) ctx.lookup("queue/MyQueue");
+                                                                          
+       // lookup the queue connection factory
+       QueueConnectionFactory connFactory = (QueueConnectionFactory) ctx.lookup("XAConnectionFactory");
+                                                                          
+       // create a queue connection
+       QueueConnection queueConn = connFactory.createQueueConnection();
+                                                                          
+       // create a queue session
+       QueueSession queueSession = queueConn.createQueueSession(false,
+           Session.DUPS_OK_ACKNOWLEDGE);
+                                                                          
+       // create a queue sender
+       QueueSender queueSender = queueSession.createSender(queue);
+       queueSender.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+                                                                          
+       //create a simple message to say "Hello"
+       TextMessage message = queueSession.createTextMessage("Hello");
+                                                                          
+       // send the message
+       queueSender.send(message);
+       // print what we did
+       System.out.println("sent: " + message.getText());
+/*                                                       
+       //-----------------------------------------------------------------------
+       BytesMessage message1 = queueSession.createBytesMessage();
+       FileInputStream fileInputStream = new FileInputStream("TransferFile");
+       BufferedInputStream bufferedInput = new BufferedInputStream(fileInputStream);
+       message1.setObjectProperty("JMS_JBM_InputStream", bufferedInput);
+       queueSender.send(message1);
+       //-----------------------------------------------------------------------
+
+       
+               */                                                           
+       // close the queue connection
+    	queueConn.close();
+    }
+}
---
+++ trunk/SendJMS/ejbModule/com/jms/receiver/ClientJMS.java
@@ -0,0 +1,61 @@
+package com.jms.receiver;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.ejb.MessageDrivenContext;
+import javax.jms.BytesMessage;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ * Message-Driven Bean implementation class for: ClientJMS
+ *
+ */
+@MessageDriven(
+		activationConfig = { @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"), 
+				@ActivationConfigProperty(propertyName = "destination", propertyValue = "queue/MyQueue")})
+public class ClientJMS implements MessageListener {
+
+    /**
+     * Default constructor. 
+     */
+    public ClientJMS() {
+        // TODO Auto-generated constructor stub
+    }
+    MessageDrivenContext mdc;
+	/**
+     * @see MessageListener#onMessage(Message)
+     */
+    public void onMessage(Message message) {
+        // TODO Auto-generated method stub
+    	//TextMessage msg = (TextMessage) message;
+    	//System.out.println("Received:"+msg.toString());
+    	
+    	
+		try {
+			BytesMessage messageReceived = (BytesMessage)message;
+	    	File outputFile = new File("huge_message_received.dat");
+	    	FileOutputStream fileOutputStream;
+			fileOutputStream = new FileOutputStream(outputFile);
+			BufferedOutputStream bufferedOutput = new BufferedOutputStream(fileOutputStream);
+			messageReceived.setObjectProperty("JMS_JBM_SaveStream", bufferedOutput);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	
+        
+    }
+
+}
