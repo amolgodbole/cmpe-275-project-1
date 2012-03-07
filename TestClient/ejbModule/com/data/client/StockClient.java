bf4ce8b69755d5fce4e4128a5812a3dc4fea8245
nandishsb@gmail.com <nandishsb@gmail.com@50490843-ca8c-0344-2283-0fafcc131a05>
2012-03-07 02:10:11 +0000

    working client

--- TestClient/ejbModule/com/data/client/StockClient.java
+++ TestClient/ejbModule/com/data/client/StockClient.java
@@ -6,10 +6,8 @@ import javax.naming.Context;
 import javax.naming.NamingException;
  
 import com.data.business.StockProviderRemote;
+import com.data.clientutility.JNDILookupClass;
 import com.data.entity.Stockdata;
-import com.ibytecode.business.IProject;
-import com.ibytecode.clientutility.JNDILookupClass;
-import com.ibytecode.entities.Project;
  
 public class StockClient {
     private static final String LOOKUP_STRING = "StockProvider/remote";
@@ -17,44 +15,20 @@ public class StockClient {
     public static void main(String[] args) {
     	StockProviderRemote bean = doLookup();
  
-    	String str = bean.getAllData("JSON");
+    	//String str = bean.getAllData("JSON");
+    	//String str = bean.getSpatialLocation("USA", "JSON");
+    	String str = bean.getSpatialSector("Technology","JSON");
     	System.out.println("Remote result:"+str);
     	
-    	List<Stockdata> l = bean.extract();
+   /* 	List<Stockdata> l = bean.getRealTimeData();
     	
     	Iterator<Stockdata> it = l.iterator();
     	while (it.hasNext())
     	{
     		System.out.println(it.next().getOpen());
     	}
-    	
-    	
-    	
-/*        Project p1 = new Project();
-        p1.setPname("Banking App");
-        p1.setPlocation("Town City");
-        p1.setDeptNo(1);
- 
-        Project p2 = new Project();
-        p2.setPname("Office Automation");
-        p2.setPlocation("Downtown");
-        p2.setDeptNo(2);
- 
-        // 3. Call business logic
-        // Saving new Projects
-        bean.saveProject(p1);
-        bean.saveProject(p2);
- 
-        // Find a Project
-        p1.setPnumber(1);
-        Project p3 = bean.findProject(p1);
-        System.out.println(p3);
- 
-        // Retrieve all projects
-        System.out.println("List of Projects:");
-        List<Project> projects = bean.retrieveAllProjects();
-        for (Project project : projects)
-            System.out.println(project);*/
+    	*/
+    
     }
  
     private static StockProviderRemote doLookup() {
